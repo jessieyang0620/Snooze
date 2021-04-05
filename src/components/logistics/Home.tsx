12bd2162f7d6be727fd7420301d64b8cf3bd6843
William Victoria <williehankvictoria@gmail.com>
2021-04-05 12:30:01 -0400

    add navbar

--- src/components/logistics/Home.tsx
+++ src/components/logistics/Home.tsx
@@ -1,9 +1,12 @@
 import React, {Component} from "react";
+import BottomNavbar from "./BottomNavbar";
 
 export default class Home extends Component {
     render() {
         return (
-            <div></div>
+            <div>
+                <BottomNavbar/>
+            </div>
         );
     }
 }

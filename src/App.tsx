4566b3a38c7389c136e44dfe02b621bb5e488c85
William Victoria <williehankvictoria@gmail.com>
2021-04-05 11:40:22 -0400

    add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,42 @@ import {Container} from "react-bootstrap";
 import Login from "./components/logistics/Login";
 import Welcome from "./components/logistics/Welcome";
 import SignUp from "./components/logistics/SignUp";
-import Dashboard from "./components/logistics/Dashboard";
+import Home from "./components/logistics/Home";
+import Today from "./components/today/Today";
+import Calendar from "./components/calendar/Calendar";
+import {task1, task2} from "./models/Task";
 
-function App() {
-    return (
-        <Container fluid="sm">
-            <Router>
-                <div>
-                    <Switch>
-                        <Route exact path="/">
-                            <Welcome/>
-                        </Route>
-                        <Route path="/login">
-                            <Login/>
-                        </Route>
-                        <Route path="/sign-up">
-                            <SignUp/>
-                        </Route>
-                        <Route path="/dashboard">
-                            <Dashboard/>
-                        </Route>
-                        <Route path="/dashboard">
-                            <Dashboard/>
-                        </Route>
-                    </Switch>
-                </div>
-            </Router>
-        </Container>
-    );
+class App extends React.Component {
+    render() {
+        return (
+            <Container fluid="sm">
+                <Router>
+                    <div>
+                        <Switch>
+                            <Route exact path="/">
+                                <Welcome/>
+                            </Route>
+                            <Route path="/login">
+                                <Login/>
+                            </Route>
+                            <Route path="/sign-up">
+                                <SignUp/>
+                            </Route>
+                            <Route path="/home">
+                                <Home/>
+                            </Route>
+                            <Route path="/today">
+                                <Today tasks={[task1, task2]} symptoms={[]}/>
+                            </Route>
+                            <Route path="/calendar">
+                                <Calendar/>
+                            </Route>
+                        </Switch>
+                    </div>
+                </Router>
+            </Container>
+        );
+    }
 }
 
 export default App;

9d73d23327dab573a295e8f295b56b3e0682d711
William Victoria <williehankvictoria@gmail.com>
2021-04-05 11:39:11 -0400

    convert to class

--- src/components/logistics/Login.tsx
+++ src/components/logistics/Login.tsx
@@ -3,24 +3,28 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import {FormControl, FormGroup, FormLabel} from "react-bootstrap";
 
-export default function Login() {
-    return (
-        <div className="Login">
-            <Form>
-                <h3>Login</h3>
+class Login extends React.Component {
+    render() {
+        return (
+            <div className="Login">
+                <Form>
+                    <h3>Login</h3>
 
-                <FormGroup>
-                    <FormLabel>Email address</FormLabel>
-                    <FormControl type="username" placeholder="Enter username"/>
-                </FormGroup>
+                    <FormGroup>
+                        <FormLabel>Email address</FormLabel>
+                        <FormControl type="username" placeholder="Enter username"/>
+                    </FormGroup>
 
-                <FormGroup>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl type="password" placeholder="Enter password"/>
-                </FormGroup>
+                    <FormGroup>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl type="password" placeholder="Enter password"/>
+                    </FormGroup>
 
-                <Button type="submit" variant="primary" href="/dashboard">Submit</Button>
-            </Form>
-        </div>
-    );
+                    <Button type="submit" variant="primary" href="/home">Submit</Button>
+                </Form>
+            </div>
+        );
+    }
 }
+
+export default Login;
